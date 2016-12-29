74ba0c2baec219b59327936c8cbb3ae8d2c00a11
Swati <swatirokade>
2016-12-29 16:21:51 +0530

    Initial check-in using eclipse.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Swati Rokade
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World using Eclipse and GitHub!!");
+	}
+
+}
